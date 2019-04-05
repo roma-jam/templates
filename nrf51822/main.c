02f273d743b5857c14189e47742ede41bc441aea
Roma Jam <jam_roma@yahoo.com>
2019-04-05 13:50:59 +0300

    + Verify two counters parallel work.

--- nrf51822/main.c
+++ nrf51822/main.c
@@ -22,6 +22,8 @@ extern void svc_call(unsigned int num, unsigned int param1, unsigned int param2,
 
 #define RTC_PRESCALER               ((LFCLK/32768) - 1)
 
+#define S1_US                       1000000
+#define HPET_VALUE                  110000
 void main()
 {
     //
@@ -41,39 +43,39 @@ void main()
 //    nrf_radio_start();
 
     /* init timer */
-//    NRF_TIMER0->TASKS_STOP     = 1;                         // Stop timer.
-//    NRF_TIMER0->MODE           = TIMER_MODE_MODE_Timer;     // Set the timer in Timer Mode.
-//    //NRF_TIMER0->MODE           = TIMER_MODE_MODE_Counter;     // Set the timer in Timer Mode.
-//    NRF_TIMER0->PRESCALER      = TIMER_PRESCALER;           // Prescaler 0 produces 16 MHz.
-//    NRF_TIMER0->BITMODE        = TIMER_BITSIZE;             // 32 bit mode.
-//    NRF_TIMER0->TASKS_CLEAR    = 1;                         // clear the task first to be usable for later.
-//
-//
-//    NRF_TIMER0->CC[0] = 1000000;
-//    NRF_TIMER0->CC[1] = 330000;
-//
-//
-//    NRF_TIMER0->INTENSET |= TIMER_INTENSET_COMPARE0_Msk | TIMER_INTENSET_COMPARE1_Msk;
-//
-//    NVIC_EnableIRQ(TIMER0_IRQn);
-//
-//    NRF_TIMER0->TASKS_START   = 1;                          // Start timer.
+    NRF_TIMER0->TASKS_STOP     = 1;                         // Stop timer.
+    NRF_TIMER0->MODE           = TIMER_MODE_MODE_Timer;     // Set the timer in Timer Mode.
+    //NRF_TIMER0->MODE           = TIMER_MODE_MODE_Counter;     // Set the timer in Timer Mode.
+    NRF_TIMER0->PRESCALER      = TIMER_PRESCALER;           // Prescaler 0 produces 16 MHz.
+    NRF_TIMER0->BITMODE        = TIMER_BITSIZE;             // 32 bit mode.
+    NRF_TIMER0->TASKS_CLEAR    = 1;                         // clear the task first to be usable for later.
+
+
+    NRF_TIMER0->CC[0] = S1_US;
+    NRF_TIMER0->CC[1] = HPET_VALUE;
+
+    NRF_TIMER0->INTENSET |= TIMER_INTENSET_COMPARE0_Msk;
+    NRF_TIMER0->INTENSET |= TIMER_INTENSET_COMPARE1_Msk;
+
+    NVIC_EnableIRQ(TIMER0_IRQn);
+
+    NRF_TIMER0->TASKS_START   = 1;                          // Start timer.
 
     // --------------------- RTC ------------------------
-    NRF_RTC0->TASKS_STOP = 1;
-    NRF_RTC0->PRESCALER = RTC_PRESCALER;      // Set prescaler to a TICK of RTC_FREQUENCY.
-//    NVIC_SetPriority(RTC1_IRQn,PRIORITY_RTC1_IRQn);
-    NVIC_EnableIRQ(RTC0_IRQn);                // Enable Interrupt for RTC1 in the core.
-
-    NRF_RTC0->CC[0]    = 32767;
-
-    NRF_RTC0->EVENTS_COMPARE[0] = 0;
-
-    NRF_RTC0->EVTENSET |= RTC_INTENSET_COMPARE0_Msk;
-
-    NRF_RTC0->INTENSET |= RTC_INTENSET_COMPARE0_Msk;
-
-    NRF_RTC0->TASKS_START = 1;
+//    NRF_RTC0->TASKS_STOP = 1;
+//    NRF_RTC0->PRESCALER = RTC_PRESCALER;      // Set prescaler to a TICK of RTC_FREQUENCY.
+////    NVIC_SetPriority(RTC1_IRQn,PRIORITY_RTC1_IRQn);
+//    NVIC_EnableIRQ(RTC0_IRQn);                // Enable Interrupt for RTC1 in the core.
+//
+//    NRF_RTC0->CC[0]    = 32767;
+//
+//    NRF_RTC0->EVENTS_COMPARE[0] = 0;
+//
+//    NRF_RTC0->EVTENSET |= RTC_INTENSET_COMPARE0_Msk;
+//
+//    NRF_RTC0->INTENSET |= RTC_INTENSET_COMPARE0_Msk;
+//
+//    NRF_RTC0->TASKS_START = 1;
 
 
     while(1);
@@ -102,19 +104,19 @@ void TIMER0_IRQHandler()
 {
     if ((NRF_TIMER0->EVENTS_COMPARE[0] != 0) && ((NRF_TIMER0->INTENSET & TIMER_INTENSET_COMPARE0_Msk) != 0))
     {
-        printf("TIMER SECOND, tc %d\n", NRF_TIMER0->CC[0]);
+        printf("second, tc %d, hpet value %d\n", NRF_TIMER0->CC[0], NRF_TIMER0->CC[1]);
         pin_toggle(P28);
         NRF_TIMER0->EVENTS_COMPARE[0] = 0;           // Clear compare register 0 event
-        NRF_TIMER0->TASKS_CLEAR = 1;
+        NRF_TIMER0->CC[0] += S1_US;
+//        NRF_TIMER0->TASKS_CLEAR = 1;
 //        NRF_TIMER0->CC[0] = 0;
     }
 
     if ((NRF_TIMER0->EVENTS_COMPARE[1] != 0) && ((NRF_TIMER0->INTENSET & TIMER_INTENSET_COMPARE1_Msk) != 0))
     {
-        printf("TIMER HPET, tc %d\n", NRF_TIMER0->CC[1]);
+        printf("hpet, tc %d, sec value %d\n", NRF_TIMER0->CC[1], NRF_TIMER0->CC[0]);
 //        pin_toggle(P28);
         NRF_TIMER0->EVENTS_COMPARE[1] = 0;           // Clear compare register 0 event
-//        NRF_TIMER0->TASKS_CLEAR = 1;
-//        NRF_TIMER0->CC[0] = 0;
+        NRF_TIMER0->CC[1] += HPET_VALUE;
     }
 }
